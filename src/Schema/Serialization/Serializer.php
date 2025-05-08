133e35b8d60d80e01540785368d2ef0e4943f991
Adam Averay <adam@averay.com>
2025-05-08 10:19:12 +0100

    Fix handling empty cells

--- src/Schema/Serialization/Serializer.php
+++ src/Schema/Serialization/Serializer.php
@@ -6,6 +6,7 @@ namespace Averay\Csv\Schema\Serialization;
 use Averay\Csv\Exceptions\InvalidCellException;
 use Averay\Csv\Exceptions\InvalidRowException;
 use Averay\Csv\Schema\Schema;
+use Averay\Csv\Schema\Types\TypeInterface;
 
 final readonly class Serializer implements DeserializerInterface, SerializerInterface
 {
@@ -15,16 +16,33 @@ final readonly class Serializer implements DeserializerInterface, SerializerInte
   {
     $deserializedRecord = [];
     foreach ($this->schema->columns as $columnName => $columnType) {
-      if (!\array_key_exists($columnName, $record)) {
-        throw new InvalidRowException(\sprintf('Missing column "%s".', $columnName), $row);
-      }
-      try {
-        $deserializedRecord[$columnName] = $columnType->deserialize($record[$columnName]);
-      } catch (\UnexpectedValueException $exception) {
-        throw new InvalidCellException($exception->getMessage(), $columnName, $row, previous: $exception);
-      }
+      $value =
+        $record[$columnName] ?? throw new InvalidRowException(\sprintf('Missing column "%s".', $columnName), $row);
+      $deserializedRecord[$columnName] = $this->deserializeCell($value, $columnName, $columnType, $row);
     }
     return $deserializedRecord;
+  }
+
+  private function deserializeCell(string $value, string $columnName, TypeInterface $columnType, ?int $row): mixed
+  {
+    if ($value === '') {
+      // Empty cell
+      $defaultValue = $columnType->default;
+      if ($defaultValue === null && !$columnType->nullable) {
+        throw new InvalidCellException(
+          \sprintf('Missing value for non-nullable column "%s".', $columnName),
+          $columnName,
+          $row,
+        );
+      }
+      return $defaultValue;
+    }
+
+    try {
+      return $columnType->deserialize($value);
+    } catch (\UnexpectedValueException $exception) {
+      throw new InvalidCellException($exception->getMessage(), $columnName, $row, previous: $exception);
+    }
   }
 
   public function serializeRecord(array $record): array
