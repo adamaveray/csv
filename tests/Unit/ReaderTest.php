f041717dfd3987624fd371322bebbb5514b2611c
Adam Averay <adam@averay.com>
2025-05-08 10:24:18 +0100

    Add test coverage for incomplete values

--- tests/Unit/ReaderTest.php
+++ tests/Unit/ReaderTest.php
@@ -3,6 +3,9 @@ declare(strict_types=1);
 
 namespace Averay\Csv\Tests\Unit;
 
+use Averay\Csv\Exceptions\InvalidCellException;
+use Averay\Csv\Exceptions\InvalidHeaderException;
+use Averay\Csv\Exceptions\InvalidRowException;
 use Averay\Csv\HeaderFormattingTrait;
 use Averay\Csv\Reader;
 use Averay\Csv\Schema\Schema;
@@ -73,7 +76,8 @@ final class ReaderTest extends TestCase
   {
     $tz = new \DateTimeZone('Antarctica/McMurdo');
     $utc = new \DateTimeZone('UTC');
-    yield 'CSV' => [
+
+    yield 'Complete CSV' => [
       [
         [
           'string' => 'Hello World',
@@ -109,5 +113,161 @@ final class ReaderTest extends TestCase
         'serialized' => new Types\SerializedType(),
       ]),
     ];
+
+    yield 'Nullable fields' => [
+      [
+        [
+          'string' => null,
+          'int' => null,
+          'float' => null,
+          'bool' => null,
+          'datetime' => null,
+          'date' => null,
+          'time' => null,
+          'list' => null,
+          'json' => null,
+          'serialized' => null,
+        ],
+      ],
+      <<<'CSV'
+      string,int,float,bool,datetime,date,time,list,json,serialized
+      ,,,,,,,,,
+      CSV
+      ,
+      new Schema([
+        'string' => new Types\StringType(nullable: true),
+        'int' => new Types\IntType(nullable: true),
+        'float' => new Types\FloatType(nullable: true),
+        'bool' => new Types\BoolType(nullable: true),
+        'datetime' => new Types\DateTimeType(nullable: true, timezone: $tz),
+        'date' => new Types\DateType(nullable: true),
+        'time' => new Types\TimeType(nullable: true),
+        'list' => new Types\ListType(nullable: true),
+        'json' => new Types\JsonType(nullable: true),
+        'serialized' => new Types\SerializedType(nullable: true),
+      ]),
+    ];
+
+    yield 'Default values' => [
+      [
+        [
+          'string' => 'Hello World',
+          'int' => 123,
+          'float' => 123.45,
+          'bool' => true,
+          'datetime' => new \DateTimeImmutable('2025-01-01T01:23:45Z', $tz),
+          'date' => new \DateTimeImmutable('2025-01-01T00:00:00Z', $utc),
+          'time' => new \DateTimeImmutable('1970-01-01T01:23:45Z', $utc),
+          'list' => ['one', 'two', 'three'],
+          'json' => ['one' => 'two'],
+          'serialized' => ['hello' => 'world'],
+        ],
+      ],
+      <<<'CSV'
+      string,int,float,bool,datetime,date,time,list,json,serialized
+      ,,,,,,,,,
+      CSV
+      ,
+      new Schema([
+        'string' => new Types\StringType(nullable: true, defaultValue: 'Hello World'),
+        'int' => new Types\IntType(nullable: true, defaultValue: 123),
+        'float' => new Types\FloatType(nullable: true, defaultValue: 123.45),
+        'bool' => new Types\BoolType(nullable: true, defaultValue: true),
+        'datetime' => new Types\DateTimeType(
+          nullable: true,
+          defaultValue: new \DateTimeImmutable('2025-01-01T01:23:45Z', $tz),
+          timezone: $tz,
+        ),
+        'date' => new Types\DateType(
+          nullable: true,
+          defaultValue: new \DateTimeImmutable('2025-01-01T00:00:00Z', $utc),
+        ),
+        'time' => new Types\TimeType(
+          nullable: true,
+          defaultValue: new \DateTimeImmutable('1970-01-01T01:23:45Z', $utc),
+        ),
+        'list' => new Types\ListType(nullable: true, defaultValue: ['one', 'two', 'three']),
+        'json' => new Types\JsonType(nullable: true, defaultValue: ['one' => 'two']),
+        'serialized' => new Types\SerializedType(nullable: true, defaultValue: ['hello' => 'world']),
+      ]),
+    ];
+  }
+
+  public function testSchemaRejectsIncompleteHeader(): void
+  {
+    $csv = <<<'CSV'
+    one,two,three
+    a,b,c
+    CSV;
+
+    $reader = Reader::createFromString($csv)->setSchema(
+      new Schema([
+        'one' => new Types\StringType(),
+        'two' => new Types\StringType(),
+        'three' => new Types\StringType(),
+        'four' => new Types\StringType(),
+      ]),
+    );
+
+    $this->expectException(InvalidHeaderException::class);
+    $reader->toArray();
+  }
+
+  public function testSchemaRejectsMismatchedHeader(): void
+  {
+    $csv = <<<'CSV'
+    one,two,three
+    a,b,c
+    CSV;
+
+    $reader = Reader::createFromString($csv)->setSchema(
+      new Schema([
+        'one' => new Types\StringType(),
+        'two' => new Types\StringType(),
+        'foo' => new Types\StringType(),
+      ]),
+    );
+
+    $this->expectException(InvalidHeaderException::class);
+    $reader->toArray();
+  }
+
+  public function testSchemaRejectsIncompleteRow(): void
+  {
+    $csv = <<<'CSV'
+    one,two,three,four
+    a,b
+    CSV;
+
+    $reader = Reader::createFromString($csv)->setSchema(
+      new Schema([
+        'one' => new Types\StringType(),
+        'two' => new Types\StringType(),
+        'three' => new Types\StringType(),
+        'four' => new Types\StringType(),
+      ]),
+    );
+
+    $this->expectException(InvalidRowException::class);
+    $reader->toArray();
+  }
+
+  public function testSchemaRejectsEmptyNonNullableCell(): void
+  {
+    $csv = <<<'CSV'
+    one,two,three
+    a,b,
+    CSV;
+
+    $reader = Reader::createFromString($csv)->setSchema(
+      new Schema([
+        'one' => new Types\StringType(),
+        'two' => new Types\StringType(),
+        'three' => new Types\StringType(),
+      ]),
+    );
+
+    $this->expectException(InvalidCellException::class);
+    $reader->toArray();
   }
 }
